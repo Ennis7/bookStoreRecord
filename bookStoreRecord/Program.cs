e5380e55d35c0702efea4588fe867b6444593a82
Sarah <ennis7@mail.nmc.edu>
2024-03-07 20:12:21 -0500

    commit message

---
+++ bookStoreRecord/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace bookStoreRecord;
+class Program
+{
+    public record Bookstore(int ID, string Title, string Author, double Price);
+       
+    public static void Main(string[] args)
+    {
+        Bookstore book = new(1, "Midnight Library", "Matt Haig", 13.48);
+        Console.WriteLine(book);
+
+        Bookstore book2 = new(2, "Project Hail Mary", "Andy Weir", 13.98);
+        Console.WriteLine(book2);
+
+        Bookstore book3 = new(3, "How to Stop Time", "Matt Haig", 12.48);
+        Console.WriteLine(book3);
+        Console.ReadKey();
+    }
+}
+
